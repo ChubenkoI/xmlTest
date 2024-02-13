002b9f1f7289b48d86e7444760a3d16b540692bc
IgorChu <ichuben222@gmail.com>
2024-02-13 11:56:13 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+</script>
+
+<p>
+	Description list from <a href="https://dummyjson.com/docs/products">
+		https://dummyjson.com/docs/products</a>
+</p>
+<div><img src={data.products.thumbnail} alt = "thumbnailFromList" /></div>
+<div>{data.products.title}</div>
+<div>{data.products.price}</div> 
+<div>{data.products.description}</div>
+<div>{data.products.title}</div>
+
+{#each data.products.images as imageSrs}
+	<div><img src={imageSrs} alt = "productImage" /></div>
+{/each}	
