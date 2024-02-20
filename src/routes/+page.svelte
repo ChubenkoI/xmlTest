17ba294be02c138a022c85113355e3e98d09a663
IgorChu <ichuben222@gmail.com>
2024-02-20 11:57:05 +0200

    style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,9 @@
 	// import { TinySlider } from "svelte-tiny-slider"
 	/** @type {import('./$types').PageData} */
 	export let data;
-	/** git commit -m "commitname"
+	/** 
 		git add .
+		git commit -m "commitname"
 		git push */
 </script>
 
@@ -14,34 +15,15 @@
 	Description list from <a href="https://dummyjson.com/docs/products">
 		https://dummyjson.com/docs/products</a>
 </p>
-	{#each data.products as product} 
+	<!-- {#each data.products as product} 
 		<div><img src={product.thumbnail} alt = "thumbnailFromList" /></div>
 		<div>{product.title}</div>
 		<div>{product.price}</div> 
 		<div>{product.description}</div>
 		<div>{product.title}</div>
-	{/each}
-
-
-
-<!-- <TinySlider>
-	{#each data.products.images as item}
-	  <img src={item} alt="" />
-	{/each}
+	{/each} -->	
   
-	<div slot="controls" let:setIndex let:currentIndex>
-	  {#each items as _, i}
-		<button
-		  class:active={i == currentIndex}
-		  on:click={() => setIndex(i)}
-		  on:focus={() => setIndex(i)}>
-		  <img src={item} alt="" height=60 />
-		</button>
-	  {/each}
-	</div>
-  </TinySlider> -->
-  
-  <!-- <div class="overflow-x-auto">
+   <div class="overflow-x-auto">
 	<table class="table">
 	 
 	  <thead>
@@ -51,15 +33,15 @@
 			  <input type="checkbox" class="checkbox" />
 			</label>
 		  </th>
-		  <th>Name</th>
-		  <th>Job</th>
-		  <th>Favorite Color</th>
+		  <th>Title</th>
+		  <th>Prise</th>
+		  <th>Description</th>
 		  <th></th>
 		</tr>
 	  </thead>
 	  <tbody>
 		
-		{#each data.products.images as imageSrs} 
+		{#each data.products as product} 
 		
 		<tr>
 			<th>
@@ -71,23 +53,31 @@
 			  <div class="flex items-center gap-3">
 				<div class="avatar">
 				  <div class="mask mask-squircle w-12 h-12">
-					<img src={imageSrs} alt="Avatar Tailwind CSS Component" />
+					<img src={product.thumbnail} alt="Avatar Tailwind CSS Component" />
 				  </div>
 				</div>
 				<div>
-				  <div class="font-bold">Hart Hagerty</div>
-				  <div class="text-sm opacity-50">United States</div>
+				  <div class="font-bold">{product.title}</div>
+				  <div class="text-sm opacity-50">{product.category}</div>
 				</div>
 			  </div>
 			</td>
-			<td>
-			  Zemlak, Daniel and Leannon
+			<td style="text-decoration: line-through;color:red">
+				{product.price} 
+				
 			  <br/>
-			  <span class="badge badge-ghost badge-sm">Desktop Support Technician</span>
+			<span class="badge badge-ghost badge-sm">
+				{product.price - (product.price / 100) * product.discountPercentage } $</span>
 			</td>
-			<td>Purple</td>
+			<td>{product.description}</td>
 			<th>
-			  <button class="btn btn-ghost btn-xs">details</button>
+				<div class="rating">
+					<input type="radio" name="rating-4" class="mask mask-star-2 bg-green-500" />
+					<input type="radio" name="rating-4" class="mask mask-star-2 bg-green-500"  />
+					<input type="radio" name="rating-4" class="mask mask-star-2 bg-green-500" checked/>
+					<input type="radio" name="rating-4" class="mask mask-star-2 bg-green-500" />
+					<input type="radio" name="rating-4" class="mask mask-star-2 bg-green-500" />
+				  </div>
 			</th>
 		  </tr>
 		{/each}	
@@ -96,13 +86,13 @@
 	  <tfoot>
 		<tr>
 		  <th></th>
-		  <th>Name</th>
-		  <th>Job</th>
-		  <th>Favorite Color</th>
+		  <th>Title</th>
+		  <th>Prise</th>
+		  <th>Description</th>
 		  <th></th>
 		</tr>
 	  </tfoot>  
 	</table>
-  </div> -->
+  </div>
 
 	
