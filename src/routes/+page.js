ff9993c024018291927b61624f2fbcbfd814e0ee
IgorChu <ichuben222@gmail.com>
2024-03-05 11:52:47 +0200

    add count

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -2,6 +2,11 @@
 export async function load({ fetch }) {
 	const request = await fetch("https://dummyjson.com/products?limit=10");
 	const response = await request.json();
+	
+
+	response.products.forEach(product => {
+		product.count = 1;
+	});
 	console.log('response', response);
 	return {
 		products: response.products
