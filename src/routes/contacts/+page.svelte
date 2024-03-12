e124b093d7a888c614792951660057613eff0286
IgorChu <ichuben222@gmail.com>
2024-03-12 22:38:44 +0200

    add contacts page

---
+++ src/routes/contacts/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+</script>
+
+<div class="stats shadow">
+  
+	<div class="stat place-items-center">
+	  <div class="stat-title">Products</div>
+	  <div class="stat-value">31K</div>
+	  <div class="stat-desc">From January 1st to February 1st</div>
+	</div>
+	
+	<div class="stat place-items-center">
+	  <div class="stat-title">Sellers</div>
+	  <div class="stat-value text-secondary">4,200</div>
+	  <div class="stat-desc text-secondary">↗︎ 40 (2%)</div>
+	</div>
+	
+	<div class="stat place-items-center">
+	  <div class="stat-title">Number of purchases</div>
+	  <div class="stat-value">40000</div>
+	  <div class="stat-desc">↘︎ 90 (14%)</div>
+	</div>
+	
+  </div>
+<div> Почта: HHH@gmail.com</div>
+<div>Телефон: 1112558743</div>
