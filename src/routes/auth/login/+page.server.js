858442a26bac4a4b8a868b3064a96f9d84feed94
IgorChu <ichuben222@gmail.com>
2024-03-19 11:56:13 +0200

    login functional

---
+++ src/routes/auth/login/+page.server.js
@@ -0,0 +1,45 @@
+const users = [
+    {
+        email: "test1@gmail.com",
+        password: "1"
+    },
+    {
+        email: "test2@gmail.com",
+        password: "123456"
+    }
+]
+let loggetUser = {};
+/** @type {import('./$types').PageServerLoad} */
+export async function load() {
+    return {users, user: loggetUser};
+};
+
+function validateEmail(email) {
+	var re = /\S+@\S+\.\S+/;
+	return re.test(email);
+}
+
+export const actions = {
+    login: async(event)=>{
+        const formData = Object.fromEntries(await event.request.formData());    
+        console.log("Data", formData);
+        if (validateEmail(formData.email)){
+            const findUser = users.find((users)=>users.email === formData.email)
+            console.log("finduser", findUser)
+            if(findUser !== undefined && formData.password === findUser?.password){
+                loggetUser = findUser
+            }
+        }
+    }
+} 
+    register: async(event)=>{
+        const formData = Object.fromEntries(await event.request.formData());    
+        console.log("Data", formData);
+        if (validateEmail(formData.email)){
+            const findUser = users.find((users)=>users.email === formData.email)
+            console.log("finduser", findUser)
+            if(findUser !== undefined && formData.password === findUser?.password){
+                loggetUser = findUser
+            }
+        }
+    }
